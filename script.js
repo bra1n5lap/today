4ac96dc8ded9f428bef0c63c0597f8fa216cc52b
Shubhendu Singh <shubhendusingh1505@gmail.com>
2023-07-09 08:43:03 +0000

    done

---
+++ script.js
@@ -0,0 +1,109 @@
+if (localStorage.getItem('note') === null) {
+
+    var default_txt = "car driving car loan car payment FRM study FRM form fill";
+  
+    localStorage.setItem('note', default_txt);
+  
+  
+  }
+  
+  
+  
+  
+  function saveTxt() {
+  
+  
+    var txt = document.getElementById("note").value;
+  
+    /* document.getElementById("op").innerText = txt;*/
+  
+    localStorage.setItem('note', txt);
+  
+  
+  }
+  
+  
+  document.getElementById("note").innerHTML = localStorage.getItem('note');
+  
+  function freq(string) {
+    var string = string.replaceAll('\n\n', ' ');
+    var string = string.replaceAll('\n', ' ');
+  
+    var words = string.replace(/[.]/g, '').split(/\s/);
+    var freqMap = {};
+    words.forEach(function(w) {
+      if (!freqMap[w]) {
+        freqMap[w] = 0;
+      }
+      freqMap[w] += 1;
+    });
+  
+  
+    // Step - 1
+    // Create the array of key-value pairs
+    var items = Object.keys(freqMap).map(
+      (key) => {
+        return [key, freqMap[key]]
+      });
+  
+    // Step - 2
+    // Sort the array based on the second element (i.e. the value)
+    items.sort(
+      /*   (first, second) => { return first[1] - second[1] } */
+      (first, second) => {
+        return second[1] - first[1]
+      }
+  
+    );
+  
+    // Step - 3
+    // Obtain the list of keys in sorted order of the values.
+    var sorted = items.map(
+      (e) => {
+        return e[0]
+      });
+  
+    /* console.log(sorted) */
+  
+    return sorted;
+  }
+  
+  
+  function displayTxt() {
+  
+    var txt = localStorage.getItem('note');
+  
+    var f = freq(txt);
+  
+    var task = txt.split('\n');
+  
+    var msg = ""
+  
+    for (i = 0; i < f.length; i++) {
+  
+      var op = '';
+  
+      /*  console.log(f[i]); */
+      for (j = 0; j < task.length; j++) {
+        if (task[j].includes(f[i])) {
+          op = op + task[j] + '<br>';
+        }
+      }
+  
+    /*   console.log(op); */
+  
+      var t = "<details><summary>" + f[i] + "</summary>" + op + "</details>";
+      msg = msg + t;
+  
+    }
+  
+    /* console.log(f); */
+    /*  console.log(task); */
+  
+    document.getElementById("op").innerHTML = msg;
+  
+  
+  }
+  
+  displayTxt();
+  
